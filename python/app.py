641c4048f5100b537cb7705e0d7e73754676738a
Mike Harder <mharder@microsoft.com>
2020-09-15 17:05:36 -0700

    Add shared session to Python repro

--- python/app.py
+++ python/app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from azure.storage.blob import BlobClient
 
 connection_string = os.getenv("STORAGE_CONNECTION_STRING")
@@ -52,3 +53,37 @@ client2.upload_blob([], overwrite=True)
 # TCP	65375 → 443 [ACK] Seq=1203 Ack=6085 Win=262912 Len=0
 # TCP	443 → 65376 [FIN, ACK] Seq=6084 Ack=1203 Win=261888 Len=0
 # TCP	65376 → 443 [ACK] Seq=1203 Ack=6085 Win=262912 Len=0
+
+session = requests.session()
+
+client3 = BlobClient.from_connection_string(connection_string, "test", "test", session=session)
+print("BlobClient.upload_blob #3")
+client3.upload_blob([], overwrite=True)
+
+client4 = BlobClient.from_connection_string(connection_string, "test", "test", session=session)
+print("BlobClient.upload_blob #4")
+client4.upload_blob([], overwrite=True)
+
+# Wireshark shows one HTTPS connection was used
+# 
+# TCP	63612 → 443 [SYN] Seq=0 Win=64240 Len=0 MSS=1460 WS=256 SACK_PERM=1
+# TCP	443 → 63612 [SYN, ACK] Seq=0 Ack=1 Win=8192 Len=0 MSS=1440 WS=256 SACK_PERM=1
+# TCP	63612 → 443 [ACK] Seq=1 Ack=1 Win=263424 Len=0
+# TLSv1.2	Client Hello
+# TCP	443 → 63612 [ACK] Seq=1 Ack=518 Win=262656 Len=1460 [TCP segment of a reassembled PDU]
+# TCP	443 → 63612 [ACK] Seq=1461 Ack=518 Win=262656 Len=1460 [TCP segment of a reassembled PDU]
+# TCP	63612 → 443 [ACK] Seq=518 Ack=2921 Win=263424 Len=0
+# TCP	443 → 63612 [ACK] Seq=2921 Ack=518 Win=262656 Len=1460 [TCP segment of a reassembled PDU]
+# TLSv1.2	Server Hello, Certificate, Server Key Exchange, Server Hello Done
+# TCP	63612 → 443 [ACK] Seq=518 Ack=5544 Win=263424 Len=0
+# TLSv1.2	Client Key Exchange, Change Cipher Spec, Encrypted Handshake Message
+# TCP	443 → 63612 [ACK] Seq=5544 Ack=644 Win=262656 Len=0
+# TCP	[TCP Dup ACK 410#1] 443 → 63612 [ACK] Seq=5544 Ack=644 Win=262656 Len=0
+# TLSv1.2	Change Cipher Spec, Encrypted Handshake Message
+# TLSv1.2	Application Data
+# TLSv1.2	Application Data
+# TLSv1.2	Application Data
+# TLSv1.2	Application Data
+# TCP	63612 → 443 [FIN, ACK] Seq=1760 Ack=6573 Win=262400 Len=0
+# TCP	443 → 63612 [FIN, ACK] Seq=6573 Ack=1761 Win=261376 Len=0
+# TCP	63612 → 443 [ACK] Seq=1761 Ack=6574 Win=262400 Len=0
